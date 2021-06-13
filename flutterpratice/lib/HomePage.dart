558b60a3fb9a9c91ddba33eb3ca8480c620f062d
soorajkumarluhana <soorajkumarluhana@gmail.com>
2021-06-12 17:18:24 -0700

    done

--- flutterpratice/lib/HomePage.dart
+++ flutterpratice/lib/HomePage.dart
@@ -8,7 +8,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("CataLog App"),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text("Catalog App", style: TextStyle(color: Colors.black),),
       ),
       body: Center(child: Text("Login Page"),),
       drawer: Drawer(
